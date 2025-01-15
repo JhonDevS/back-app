460ad3c9c6f7d796509aa71975de36ce6683983f
JhonDevS <sierrazuluaga.j@gmail.com>
2025-01-14 19:53:59 -0500

    feat: integrate login with supabase

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,8 @@ import { env } from 'process';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 8002, // eslint-disable-line no-undef
-  env: env.NODE_ENV || 'development'
+  port: env.PORT || 8002,
+  env: env.NODE_ENV || 'development',
+  urlSupaBase: env.SUPABASE_URL || '',
+  keySupaBase: env.supabaseKey || ''
 };
